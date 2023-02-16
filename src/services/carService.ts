f6c5215eeccc6c8c07d6af8990c0eae203ca65a2
DeDuShKa12 <pasharudiuk97@gmail.com>
2023-02-16 20:31:43 +0200

    Lesson 1 HW 2 Done

---
+++ src/services/carService.ts
@@ -0,0 +1,15 @@
+
+import {ICar} from '../interfaces';
+import {urls} from '../configs';
+import {apiService, IRes} from "./apiService";
+
+const carService = {
+    getAll: (): IRes<ICar[]> => apiService.get(urls.cars.base),
+    getById: (id: string): IRes<ICar> => apiService.get(urls.cars.byId(id)),
+    create: (car: ICar): IRes<ICar> => apiService.post(urls.cars.base, car)
+}
+
+
+export {
+    carService
+}
